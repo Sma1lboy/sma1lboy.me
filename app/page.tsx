2bc38252cdfcb7c7012c28f56548ce96522d84fb
Jackson Chen <541898146chen@gmail.com>
2024-09-09 00:04:13 -0500

    refactor(app): use siteConfig for username in UserProfile component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import UserProfile from "@/components/user-profile";
 import { Suspense } from "react";
-
+import { siteConfig } from "@/config/site";
 export default async function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <Suspense fallback={<div>Loading user profile...</div>}>
-        <UserProfile username="Sma1lboy" />
+        <UserProfile username={siteConfig.name} />
       </Suspense>
     </section>
   );
