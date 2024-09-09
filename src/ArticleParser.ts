89cb9baab63f95b3aed7ea7f30664b2e753f99cf
Jackson Chen <541898146chen@gmail.com>
2024-09-08 21:35:35 -0500

    chore: set up initial project structure for Next.js
    - Added .eslintrc.json for ESLint configuration.
    - Updated .gitignore to include common Next.js and development files.
    - Updated README.md with Next.js project setup instructions.
    - Added next.config.mjs for Next.js configuration.
    - Added favicon.ico to the src/app directory.
    - Added postcss.config.mjs for PostCSS configuration.
    - Added GeistMonoVF.woff font file to src/app/fonts directory.

--- src/ArticleParser.ts
+++
@@ -1,8 +0,0 @@
-// speical markdown parser for article
-
-import content from "./contents/content";
-export function process() {
-    return content;
-}
-let data = process();
-export default data;
--- src/contents/content.ts
+++
@@ -1,9 +0,0 @@
-let content = {
-
-        author: "Jackson Chen",
-        article: [
-                "I am a Full Stack Developer and also student in University of Wisconsin-Madison major in Computer Science. I'm an explorer of new tech, an avid learner, and a problem-solver at heart. Moreover, the passionate programmer on the earth. Feel free to connect with me for all things tech or just to say hello! Let's shape the future of tech together. 🌟 Since then I pushed 679+ commits, opened 9+ issues, submitted 21+ pull requests, created 0+ gists and contributed to 25+ public repositories",
-        ]
-    
-}
-export default content
