17b20754a2b8deff0cf5b281fd78939df4ef57b6
Jackson Chen <541898146chen@gmail.com>
2024-09-30 22:13:27 -0500

    refactor: update MarkdownNote component to specify the type of children.undefined

--- components/MarkdownProvider.tsx
+++ components/MarkdownProvider.tsx
@@ -1,7 +1,7 @@
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
-export const MarkdownNote = ({ children }) => {
+export const MarkdownNote = ({ children }: { children: string }) => {
   return (
     <Markdown
       className="prose prose-stone flex flex-col justify-center dark:prose-invert"
--- utils/MarkdownUtils.tsx
+++ utils/MarkdownUtils.tsx
@@ -7,6 +7,9 @@ import parseMD from 'parse-md'
 
 import { Blog } from '@/app/blog/Blog'
 
+interface BlogMetadata {
+  Date?: string
+}
 export class MarkdownUtils {
   private static readonly BLOG_BASE_URL = './public/blog'
   private static readonly PREFIX_MAX_CHARS = 80
@@ -23,6 +26,15 @@ export class MarkdownUtils {
     return readdirSync(dir).filter(file => file.endsWith('.md'))
   }
 
+  private static isValidMetadata(metadata: unknown): metadata is BlogMetadata {
+    return (
+      typeof metadata === 'object' &&
+      metadata !== null &&
+      'Date' in metadata &&
+      (typeof (metadata as BlogMetadata).Date === 'string' ||
+        (metadata as BlogMetadata).Date === undefined)
+    )
+  }
   private static getGitLastModifiedDate(filePath: string): string {
     try {
       const dateStr = execSync(`git log -1 --format=%cd -- "${filePath}"`, {
@@ -61,8 +73,12 @@ export class MarkdownUtils {
       const slug = MarkdownUtils.convertMarkdownNameToSlug(file)
       const { metadata, content } = parseMD(data)
 
-      const date = new Date(metadata['Date'] ? metadata['Date'] : Date.now())
-
+      let date: Date
+      if (MarkdownUtils.isValidMetadata(metadata) && metadata.Date) {
+        date = new Date(metadata.Date)
+      } else {
+        date = new Date()
+      }
       return {
         date: date.toISOString(),
         description,
