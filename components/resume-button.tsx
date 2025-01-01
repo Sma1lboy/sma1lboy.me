b7c07e26ee2ca2690331853cd6fcdb490629b966
Jackson Chen <541898146chen@gmail.com>
2024-12-31 21:59:40 -0600

    feat: update ResumeButton to link to resume PDF and add new dependencies

--- components/resume-button.tsx
+++ components/resume-button.tsx
@@ -4,11 +4,13 @@ import { defaultTheme } from '@/app/generateTheme'
 
 export const ResumeButton = () => {
   return (
-    <PulsatingButton
-      className="inline-flex flex-row items-center whitespace-nowrap rounded-md border border-primary bg-white px-4 py-2 text-sm font-medium text-primary shadow-sm hover:bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
-      pulseColor={defaultTheme['light'].colors.primary.DEFAULT}
-    >
-      Resume
-    </PulsatingButton>
+    <a href="/resume.pdf" rel="noopener noreferrer" target="_blank">
+      <PulsatingButton
+        className="inline-flex flex-row items-center whitespace-nowrap rounded-md border border-primary bg-white px-4 py-2 text-sm font-medium text-primary shadow-sm hover:bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+        pulseColor={defaultTheme['light'].colors.primary.DEFAULT}
+      >
+        Resume
+      </PulsatingButton>
+    </a>
   )
 }
